6eb26bef9815f367fa5668560c38fd5b8cea1d2f
oitsjustjose <stovallj1995@gmail.com>
2019-10-26 23:06:46 -0400

    Initialize SoundEvents

---
+++ src/main/java/com/oitsjustjose/naturalprogression/common/utils/Sounds.java
@@ -0,0 +1,10 @@
+package com.oitsjustjose.naturalprogression.common.utils;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
+
+public class Sounds
+{
+    public static SoundEvent CRAFT_PLANK = new SoundEvent(new ResourceLocation(Constants.MODID, "craft_plank"));
+    public static SoundEvent CRAFT_OTHER = new SoundEvent(new ResourceLocation(Constants.MODID, "craft_other"));
+}
