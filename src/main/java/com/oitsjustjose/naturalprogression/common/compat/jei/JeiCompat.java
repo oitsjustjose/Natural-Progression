31703f24f66c69a9283978b2f13d1579a3a2d40b
oitsjustjose <stovallj1995@gmail.com>
2019-11-09 21:55:44 -0600

    Descriptions for pebbles!

--- src/main/java/com/oitsjustjose/naturalprogression/common/compat/jei/JeiCompat.java
+++ src/main/java/com/oitsjustjose/naturalprogression/common/compat/jei/JeiCompat.java
@@ -1,11 +1,17 @@
 package com.oitsjustjose.naturalprogression.common.compat.jei;
 
+import java.util.ArrayList;
+
+import com.google.common.collect.Lists;
+import com.oitsjustjose.naturalprogression.common.blocks.NaturalProgressionBlocks;
 import com.oitsjustjose.naturalprogression.common.utils.Constants;
 
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.constants.VanillaRecipeCategoryUid;
+import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 
 @JeiPlugin
@@ -21,5 +27,15 @@ public class JeiCompat implements IModPlugin
     public void registerRecipes(IRecipeRegistration registration)
     {
         registration.addRecipes(PlankRecipeMaker.getRecipes(), VanillaRecipeCategoryUid.CRAFTING);
+
+        ArrayList<ItemStack> pebbles = Lists.newArrayList();
+        
+        pebbles.add(new ItemStack(NaturalProgressionBlocks.andesitePebble));
+        pebbles.add(new ItemStack(NaturalProgressionBlocks.dioritePebble));
+        pebbles.add(new ItemStack(NaturalProgressionBlocks.granitePebble));
+        pebbles.add(new ItemStack(NaturalProgressionBlocks.stonePebble));
+        pebbles.add(new ItemStack(NaturalProgressionBlocks.sandstonePebble));
+
+        registration.addIngredientInfo(pebbles, VanillaTypes.ITEM, "natural-progression.pebble.jei.info");
     }
 }
