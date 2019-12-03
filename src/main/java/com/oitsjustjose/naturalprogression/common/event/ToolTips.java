8c60d9b9058baf0a24d671d89625df59a7321574
oitsjustjose <stovallj1995@gmail.com>
2019-12-03 10:57:22 -0500

    Properly add tooltips for only tools in the right tag

--- src/main/java/com/oitsjustjose/naturalprogression/common/event/ToolTips.java
+++ src/main/java/com/oitsjustjose/naturalprogression/common/event/ToolTips.java
@@ -1,34 +1,43 @@
 package com.oitsjustjose.naturalprogression.common.event;
 
-import com.oitsjustjose.naturalprogression.common.items.SawItem;
+import java.util.ArrayList;
+
+import com.google.common.collect.Lists;
+import com.oitsjustjose.naturalprogression.common.utils.Constants;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.Tag;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.common.ToolType;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class ToolTips
 {
+    private ArrayList<Item> woodenTools = Lists.newArrayList(Items.WOODEN_AXE, Items.WOODEN_PICKAXE,
+            Items.WOODEN_SHOVEL, Items.WOODEN_HOE, Items.WOODEN_SWORD);
+    private Tag<Item> axes = ItemTags.getCollection().getOrCreate(new ResourceLocation(Constants.MODID, "axe"));
+    private Tag<Item> saws = ItemTags.getCollection().getOrCreate(new ResourceLocation(Constants.MODID, "saw"));
+
     @SubscribeEvent
     @OnlyIn(Dist.CLIENT)
     public void onItemHover(ItemTooltipEvent event)
     {
-        if (!event.getItemStack().getToolTypes().contains(ToolType.AXE))
+        if (axes.contains(event.getItemStack().getItem()))
         {
-            return;
+            event.getToolTip().add(new TranslationTextComponent("natural-progression.axe.tooltip"));
         }
-
-        TranslationTextComponent tooltip;
-        if (event.getItemStack().getItem() instanceof SawItem)
+        else if (saws.contains(event.getItemStack().getItem()))
         {
-            tooltip = new TranslationTextComponent("natural-progression.saw.tooltip");
+            event.getToolTip().add(new TranslationTextComponent("natural-progression.saw.tooltip"));
         }
-        else
+        else if (woodenTools.contains(event.getItemStack().getItem()))
         {
-            tooltip = new TranslationTextComponent("natural-progression.axe.tooltip");
+            event.getToolTip().add(new TranslationTextComponent("natural-progression.wooden.tool.tooltip"));
         }
-
-        event.getToolTip().add(tooltip);
     }
 }
