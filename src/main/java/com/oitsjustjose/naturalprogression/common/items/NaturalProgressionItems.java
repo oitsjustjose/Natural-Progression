cc5ac6cbb9cff93df662f6b8b142ac7cd048abee
oitsjustjose <stovallj1995@gmail.com>
2019-12-18 11:00:13 -0500

    Bone Knife Implementation!

--- src/main/java/com/oitsjustjose/naturalprogression/common/items/NaturalProgressionItems.java
+++ src/main/java/com/oitsjustjose/naturalprogression/common/items/NaturalProgressionItems.java
@@ -8,17 +8,22 @@ import com.oitsjustjose.naturalprogression.common.utils.NaturalProgressionGroup;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemTier;
 import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.SwordItem;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
 
 public class NaturalProgressionItems
 {
     private static ArrayList<Item> modItems = new ArrayList<>();
+    private static BoneItemTier boneTier = new BoneItemTier();
+
     public static Item flintHatchet;
     public static Item bonePickaxe;
+    public static Item boneKnife;
     public static Item boneShard;
     public static Item basicSaw;
     public static Item improvedSaw;
+
 
     public static void registerItems(final RegistryEvent.Register<Item> itemRegistryEvent)
     {
@@ -26,10 +31,15 @@ public class NaturalProgressionItems
         flintHatchet.setRegistryName(new ResourceLocation(Constants.MODID, "flint_hatchet"));
         modItems.add(flintHatchet);
 
-        bonePickaxe = new PickaxeItem(new BoneItemTier(), 1, -2.8F,
+        bonePickaxe = new PickaxeItem(boneTier, 1, -2.8F,
                 new Item.Properties().group(NaturalProgressionGroup.getInstance()).maxStackSize(1).maxDamage(40));
         bonePickaxe.setRegistryName(new ResourceLocation(Constants.MODID, "bone_pickaxe"));
         modItems.add(bonePickaxe);
+
+        boneKnife = new SwordItem(boneTier, 1, -1.4F,
+                new Item.Properties().group(NaturalProgressionGroup.getInstance()).maxStackSize(1).maxDamage(40));
+        boneKnife.setRegistryName(new ResourceLocation(Constants.MODID, "bone_knife"));
+        modItems.add(boneKnife);
 
         boneShard = new Item(new Item.Properties().group(NaturalProgressionGroup.getInstance()));
         boneShard.setRegistryName(new ResourceLocation(Constants.MODID, "bone_shard"));
