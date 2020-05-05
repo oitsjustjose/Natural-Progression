cb9ce2334c417f11db28c737b1c489dd4286e67b
Sargun Vohra <sargun.vohra@gmail.com>
2020-05-04 20:01:03 -0700

    replace generator script and regenerate recipes

--- scripts/generate.py
+++ scripts/generate.py
@@ -1,172 +1,227 @@
-"""
-Author: oitsjustjose | oitsjustjose@github
-
-A Python script for generating Natural Progression recipes
-"""
+#!/usr/bin/env python3
 
 import json
 import os
-from pip._vendor.colorama import Fore
-
-def genSawStripping(saws: list, strip_map: dict) -> None:
-    """
-    Generates recipes for stripping logs with a saw
-    """
-    for log in strip_map:
-        stripped_log = strip_map[log]
-
-        json_raw = {
-            "type": "natural-progression:damage_tools",
-            "ingredients": [
-                {
-                    "item": log
-                },
-                [{"item": saw} for saw in saws]
-            ],
-            "result": {
-                "item": stripped_log,
-                "count": 1
-            }
-        }
-
-        id_1 = log[log.index(":") + 1:]
-        id_2 = stripped_log[stripped_log.index(":") + 1:]
-
-        filename = "stripping_{}_to_{}".format(id_1, id_2)
-
-        if os.path.exists(filename):
-            addon = 0
-            while os.path.exists(filename + "_{}".format(addon)):
-                addon += 1
-            filename += "_{}".format(addon)
+from typing import NamedTuple, Set, Any, Dict, Tuple, Callable, List
 
 
-        with open(filename + ".json", "w") as file:
-            file.write(json.dumps(json_raw))
-            print(Fore.BLUE + "Wrote file {}".format(filename) + Fore.RESET)
-
-        del stripped_log
-
-def genSawPlanking(saws: list, plank_map: dict) -> None:
-    """
-    Generates recipes for saws with planks
-    """
-    for stripped_log in plank_map:
-        plank = plank_map[stripped_log]
-
-        json_raw = {
-            "type": "natural-progression:damage_tools",
-            "ingredients": [
-                {
-                    "item": stripped_log
-                },
-                [{"item": saw} for saw in saws]
-            ],
-            "result": {
-                "item": plank,
-                "count": 4
-            }
-        }
-
-        id_1 = stripped_log[stripped_log.index(":") + 1:]
-        id_2 = plank[plank.index(":") + 1:]
-
-        filename = "sawing_{}_to_{}".format(id_1, id_2)
-
-        if os.path.exists(filename):
-            addon = 0
-            while os.path.exists(filename + "_{}".format(addon)):
-                addon += 1
-            filename += "_{}".format(addon)
+class WoodType(NamedTuple):
+    log: str
+    wood: str
+    planks: str
+    stripped_log: str
+    stripped_wood: str
 
 
-        with open(filename + ".json", "w") as file:
-            file.write(json.dumps(json_raw))
-            print(Fore.BLUE + "Wrote file {}".format(filename) + Fore.RESET)
-
-        del plank
-
-def genAxePlanking(axes: list, plank_map:dict) -> None:
-    """
-    Generates recipes for axes with planks
-    """
-
-    for stripped_log in plank_map:
-        plank = plank_map[stripped_log]
-
-        json_raw = {
-            "type": "natural-progression:damage_tools",
-            "ingredients": [
-                {
-                    "item": stripped_log
-                },
-                [{"item": axe} for axe in axes]
-            ],
-            "result": {
-                "item": plank,
-                "count": 1
-            }
-        }
-
-        id_1 = stripped_log[stripped_log.index(":") + 1:]
-        id_2 = plank[plank.index(":") + 1:]
-
-        filename = "axing_{}_to_{}".format(id_1, id_2)
-
-        if os.path.exists(filename):
-            addon = 0
-            while os.path.exists(filename + "_{}".format(addon)):
-                addon += 1
-            filename += "_{}".format(addon)
+class WoodSet(NamedTuple):
+    namespace: str
+    wood_types: Set[WoodType]
 
 
-        with open(filename + ".json", "w") as file:
-            file.write(json.dumps(json_raw))
-            print(Fore.BLUE + "Wrote file {}".format(filename) + Fore.RESET)
+def vanilla_named_wood_type(name: str) -> WoodType:
+    return WoodType(
+        log=name + "_log",
+        wood=name + "_wood",
+        planks=name + "_planks",
+        stripped_log="stripped_" + name + "_log",
+        stripped_wood="stripped_" + name + "_wood",
+    )
 
-        del plank
+
+def legacy_wood_type(log: str, planks: str) -> WoodType:
+    return WoodType(
+        log=None, wood=None, planks=planks, stripped_log=log, stripped_wood=None,
+    )
 
 
-def main() -> None:
-    """Generates recipes"""
-    axes = ["minecraft:stone_axe", "minecraft:iron_axe", "minecraft:diamond_axe", "natural-progression:flint_hatchet"]
-    saws = ["natural-progression:basic_saw", "natural-progression:improved_saw"]
+def midnight_named_wood_type(name: str) -> WoodType:
+    return WoodType(
+        log=name + "_log",
+        wood=name + "_wood",
+        planks=name + "_planks",
+        stripped_log=name + "_stripped_log",
+        stripped_wood=name + "_stripped_wood",
+    )
 
-    strip_map = {
-        "minecraft:oak_log": "minecraft:stripped_oak_log",
-        "minecraft:spruce_log": "minecraft:stripped_spruce_log",
-        "minecraft:birch_log": "minecraft:stripped_birch_log",
-        "minecraft:jungle_log": "minecraft:stripped_jungle_log",
-        "minecraft:dark_oak_log": "minecraft:stripped_dark_oak_log",
-        "minecraft:acacia_log": "minecraft:stripped_acacia_log",
-        "minecraft:oak_wood": "minecraft:stripped_oak_wood",
-        "minecraft:spruce_wood": "minecraft:stripped_spruce_wood",
-        "minecraft:birch_wood": "minecraft:stripped_birch_wood",
-        "minecraft:jungle_wood": "minecraft:stripped_jungle_wood",
-        "minecraft:dark_oak_wood": "minecraft:stripped_dark_oak_wood",
-        "minecraft:acacia_wood": "minecraft:stripped_acacia_wood",
+
+def midnight_stem_type(name: str) -> WoodType:
+    return legacy_wood_type(log=name + "_stem", planks=name + "_planks")
+
+
+WOOD_SETS = [
+    WoodSet(
+        namespace="minecraft",
+        wood_types={
+            vanilla_named_wood_type("oak"),
+            vanilla_named_wood_type("spruce"),
+            vanilla_named_wood_type("birch"),
+            vanilla_named_wood_type("jungle"),
+            vanilla_named_wood_type("dark_oak"),
+            vanilla_named_wood_type("acacia"),
+        },
+    ),
+    WoodSet(
+        namespace="atmospheric",
+        wood_types={
+            vanilla_named_wood_type("rosewood")._replace(
+                wood="rosewood", stripped_wood="stripped_rosewood"
+            ),
+            vanilla_named_wood_type("yucca"),
+            vanilla_named_wood_type("kousa"),
+            vanilla_named_wood_type("aspen"),
+        },
+    ),
+    WoodSet(
+        namespace="autumnity",
+        wood_types={
+            vanilla_named_wood_type("maple"),
+            vanilla_named_wood_type("sappy_maple")._replace(
+                stripped_log="stripped_maple_log",
+                stripped_wood="stripped_maple_wood",
+                planks="maple_planks",
+            ),
+        },
+    ),
+    WoodSet(namespace="bloomful", wood_types={vanilla_named_wood_type("wisteria")}),
+    WoodSet(namespace="swampexpansion", wood_types={vanilla_named_wood_type("willow")}),
+    WoodSet(
+        namespace="botania",
+        wood_types={legacy_wood_type(log="livingwood", planks="livingwood_planks")},
+    ),
+    WoodSet(
+        namespace="midnight",
+        wood_types={
+            midnight_named_wood_type("shadowroot"),
+            midnight_named_wood_type("dark_willow"),
+            midnight_named_wood_type("dead_wood")._replace(
+                wood="dead_wood", stripped_wood="dead_wood_stripped",
+            ),
+            midnight_stem_type("bogshroom"),
+            midnight_stem_type("nightshroom"),
+            midnight_stem_type("dewshroom"),
+            midnight_stem_type("viridshroom"),
+        },
+    ),
+    WoodSet(namespace="traverse", wood_types={vanilla_named_wood_type("fir")}),
+    WoodSet(
+        namespace="upgrade_aquatic",
+        wood_types={
+            WoodType(
+                log="driftwood_log",
+                wood="driftwood",
+                planks="driftwood_planks",
+                stripped_log="driftwood_log_stripped",
+                stripped_wood="driftwood_stripped",
+            ),
+        },
+    ),
+    WoodSet(
+        namespace="endergetic",
+        wood_types={
+            WoodType(
+                log="poise_stem",
+                wood="poise_wood",
+                planks="poise_planks",
+                stripped_log="poise_stem_stripped",
+                stripped_wood="poise_wood_stripped",
+            ),
+            WoodType(
+                log="poise_stem_glowing",
+                wood="poise_wood_glowing",
+                planks="poise_planks",
+                stripped_log="poise_stem_stripped",
+                stripped_wood="poise_wood_stripped",
+            ),
+        },
+    ),
+]
+
+
+def damage_tools_recipe(
+    action: str, namespace: str, original: str, tool: str, result: str, amount: int,
+) -> Tuple[str, str]:
+    return (
+        f"data/natural-progression/recipes/wood/{action}/{namespace}/{action}_{original}_to_{result}.json",
+        json.dumps(
+            {
+                "conditions": [{"type": "forge:mod_loaded", "modid": namespace}],
+                "type": "natural-progression:damage_tools",
+                "ingredients": [
+                    {"item": f"{namespace}:{original}"},
+                    {"tag": f"natural-progression:{tool}"},
+                ],
+                "result": {"item": f"{namespace}:{result}", "count": amount},
+            },
+            indent=4,
+        ),
+    )
+
+
+def damage_tools_recipe_set(
+    action: str,
+    pairs: Callable[[WoodType], List[Tuple[str, str]]],
+    tool: str,
+    amount: int,
+) -> Set[Tuple[str, str]]:
+    return {
+        damage_tools_recipe(
+            action=action,
+            namespace=wood_set.namespace,
+            original=first,
+            tool=tool,
+            result=second,
+            amount=amount,
+        )
+        for wood_set in WOOD_SETS
+        for wood_type in wood_set.wood_types
+        for first, second in pairs(wood_type)
+        if first is not None and second is not None
     }
 
-    plank_map = {
-        "minecraft:stripped_oak_log": "minecraft:oak_planks",
-        "minecraft:stripped_spruce_log": "minecraft:spruce_planks",
-        "minecraft:stripped_birch_log": "minecraft:birch_planks",
-        "minecraft:stripped_jungle_log": "minecraft:jungle_planks",
-        "minecraft:stripped_dark_oak_log": "minecraft:dark_oak_planks",
-        "minecraft:stripped_acacia_log": "minecraft:acacia_planks",
-        "minecraft:stripped_oak_wood": "minecraft:oak_planks",
-        "minecraft:stripped_spruce_wood": "minecraft:spruce_planks",
-        "minecraft:stripped_birch_wood": "minecraft:birch_planks",
-        "minecraft:stripped_jungle_wood": "minecraft:jungle_planks",
-        "minecraft:stripped_dark_oak_wood": "minecraft:dark_oak_planks",
-        "minecraft:stripped_acacia_wood": "minecraft:acacia_planks"
-    }
 
-    genAxePlanking(axes, plank_map)
-    genSawPlanking(saws, plank_map)
-    genSawStripping(saws, strip_map)
+STRIPPING_RECIPES = damage_tools_recipe_set(
+    action="stripping",
+    pairs=lambda wood_type: [
+        (wood_type.log, wood_type.stripped_log),
+        (wood_type.wood, wood_type.stripped_wood),
+    ],
+    tool="saw",
+    amount=1,
+)
 
-    
 
-if __name__ == "__main__":
-    main()
+AXING_RECIPES = damage_tools_recipe_set(
+    action="axing",
+    pairs=lambda wood_type: [
+        (wood_type.stripped_log, wood_type.planks),
+        (wood_type.stripped_wood, wood_type.planks),
+    ],
+    tool="axe",
+    amount=1,
+)
+
+
+SAWING_RECIPES = damage_tools_recipe_set(
+    action="sawing",
+    pairs=lambda wood_type: [
+        (wood_type.stripped_log, wood_type.planks),
+        (wood_type.stripped_wood, wood_type.planks),
+    ],
+    tool="saw",
+    amount=4,
+)
+
+
+def dump(file_name: str, data: str):
+    file_name = f"src/main/resources/{name}"
+    os.makedirs(os.path.dirname(file_name), exist_ok=True)
+    with open(file_name, "w") as fp:
+        fp.write(data)
+
+
+for name, data in STRIPPING_RECIPES:
+    dump(file_name=name, data=data)
+for name, data in AXING_RECIPES:
+    dump(file_name=name, data=data)
+for name, data in SAWING_RECIPES:
+    dump(file_name=name, data=data)
