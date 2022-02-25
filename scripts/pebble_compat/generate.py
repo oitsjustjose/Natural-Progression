12134f576abf7dc952e346d971f93ad1280469a7
oitsjustjose <stovallj1995@gmail.com>
2022-02-25 10:40:33 -0600

    Add new create pebble bois

--- scripts/pebble_compat/generate.py
+++ scripts/pebble_compat/generate.py
@@ -5,27 +5,15 @@ from common.pebble_assets import PebbleAssetGenerator
 from common.pebble_drops import PebbleDropGenerator
 from common.pebble_recipes import PebbleRecipeGenerator
 
-quark_stones = ModStones(
-    "quark", ["marble", "limestone", "jasper", "slate", "basalt"]
-)
-quark_pag = PebbleAssetGenerator(quark_stones)
-quark_pdg = PebbleDropGenerator(quark_stones)
-
-# quark_pdg.generate()
-# quark_pag.generate_blockstates()
-# quark_pag.generate_block_models()
-# quark_pag.generate_item_models()
-
 create_stones = ModStones(
     "create",
-    ["limestone", "weathered_limestone", "dolomite", "gabbro", "scoria", "dark_scoria"],
+    ["asurine", "crimsite", "limestone", "ochrum", "scorchia", "scoria", "veridium"],
 )
 create_pag = PebbleAssetGenerator(create_stones)
 create_pdg = PebbleDropGenerator(create_stones)
 create_prg = PebbleRecipeGenerator(create_stones)
 
-# create_pdg.generate()
 create_prg.generate()
-# create_pag.generate_blockstates()
-# create_pag.generate_block_models()
-# create_pag.generate_item_models()
+create_pag.generate_blockstates()
+create_pag.generate_block_models()
+create_pag.generate_item_models()
